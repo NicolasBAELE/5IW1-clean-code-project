992fa981e54614b1ebcb0cc7279365c61c8595b6
jandigi <jan@digipharmacie.fr>
2025-01-23 10:10:51 +0100

    update all containers config add update the package_lock.json with node 22 same as the node in docker container

--- infrastructure/adapters/repositories/prisma/src/index.ts
+++ infrastructure/adapters/repositories/prisma/src/index.ts
@@ -1,20 +1,40 @@
 import express from "express";
-import { PrismaClient } from "@prisma/client/extension";
+import { PrismaClient } from '@prisma/client'
 
 const app = express();
 const port = 3000;
 
-const prisma = PrismaClient;
-
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+const prisma = new PrismaClient({log : ['query', 'info', 'warn', 'error'],
 });
 
-// app.get("/users", (req, res) => {
-//     res.send(prisma.users.sync);
-// });
+app.get("/motos", (req, res) => {
+    async function main() {
+        try {
+            // Requête pour récupérer les motos
+            const allMotos = await prisma.moto.findMany({
+                take: 10, // Limite à 10 résultats
+            });
 
-// Je pense que tu devras renvoyer un truc du genre quoi
+            // Envoyer les résultats au client
+            res.status(200).json(allMotos);
+        } catch (error) {
+            // Gérer les erreurs et envoyer une réponse appropriée
+            console.error("Erreur lors de la récupération des motos :", error);
+            res.status(500).json({ error: "Erreur lors de la récupération des motos." });
+        }
+    }
+
+    // Exécuter la fonction main() et gérer la déconnexion
+    main()
+        .then(async () => {
+            await prisma.$disconnect();
+        })
+        .catch(async (e) => {
+            console.error("Erreur fatale :", e);
+            await prisma.$disconnect();
+            process.exit(1); // Quitte le processus en cas d'erreur critique
+        });
+});
 
 app.listen(port, () => {
     console.log(`🚀 Prisma app ready at http://localhost:${port}`);
